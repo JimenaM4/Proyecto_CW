6bd17e8513985db45ce2eed511302b3e5079553f
Manuel V <322229851@alumno.enp.unam.mx>
2023-06-19 13:49:39 -0600

    feat: Agrega todos los avances anteriores del proyecto

---
+++ FINAL/Dynamics/PHP/inicio_sesion.php
@@ -0,0 +1,8 @@
+
+   <?php
+    $usuario=(isset($_POST['usuario']) && $_POST["usuario"] != "")? $_POST['usuario'] : false;
+    $contra=(isset($_POST['contraseña']) && $_POST["contraseña"] != "")? $_POST['contraseña'] : false;
+    $acceder=(isset($_POST['acceder']) && $_POST["acceder"] != "")? $_POST['acceder'] : false;
+   ?>
+</body>
+</html>
