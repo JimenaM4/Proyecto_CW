d3326cb23f8abc95c937eccd61b6a61bf94a5730
JimenaM4 <133259405+JimenaM4@users.noreply.github.com>
2023-06-20 10:22:11 -0600

    FEAT: añade funcionalidad al registro

--- FINAL/Dynamics/JS/registro.js
+++ FINAL/Dynamics/JS/registro.js
@@ -11,34 +11,45 @@ window.addEventListener("load", ()=>{
     acceder.addEventListener("click", ()=>{
     //sanitización
         const name = /^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+(?:\s+[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+){1,5}(?:\s+[-\sa-zA-ZáéíóúÁÉÍÓÚüÜñÑ]+)?$/i;
+        const nameT = name.test(nombre.value);
         //nombre completo, con espacios, recive mayusculas, minusculas y acéntos
-        if(!name.test(nombre.value)){
+        if(!nameT){
             alert("Verifica que el nombre este completo");
         }
         const cuenta = /^\d{9}$/i;
+        const cuenT = cuenta.test(numcuen.value);
         //numero de 9 digitos
-        if(!cuenta.test(numcuen.value)){
+        if(!cuenT){
             alert("Número de cuenta invalido");
         }
         const phone = /^(55?|52?|56?)[0-9]{8}$/i;
+        const telT = phone.test(telefono.value);
         //10 digitos
-        if(!phone.test(telefono.value)){
+        if(!telT){
             alert("Número de teléfono invalido");
         }
         const group = /^[4-6][1-6][0-9]$/i;
+        const grupT = group.test(grup.value);
         //solo acepta grupos del 410 al 669
-        if(!group.test(grup.value)){
+        if(!grupT){
             alert("Grupo invalido");
         }
         const user = /^[a-z0-9_-]{3,10}$/i;
+        const usuaT = user.test(usu.value);
         //usuarios con letras y numeros, 3 caracteres minimo, 10 maximo
-        if(!user.test(usu.value)){
+        if(!usuaT){
             alert("Usuario invalido");
         }
         const pass = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8}$/i;
+        const passwT = pass.test(password.value);
         //contraseña (8 digitos) que inicie con letras y/o numeros y al final caracteres especiales, ejemplo: J1m3n4$3
-        if(!pass.test(password.value)){
+        if(!passwT){
             alert("Contraseña invalida");
         }
+
+        if(nameT && cuenT && telT && grupT && usuaT && passwT)
+        {
+            acceder.value = 2; 
+        }else acceder.value = 1;
     });
 });
