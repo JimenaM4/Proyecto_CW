e7e9457a57a2a93528390cd8324f29ef7a975223
Isaac <322287879@alumno.enp.unam.mx>
2023-06-20 20:43:50 -0600

    feat: añade redireccion de otras páginas

--- FINAL/Dynamics/PHP/menu.php
+++ FINAL/Dynamics/PHP/menu.php
@@ -98,21 +98,27 @@
         <!-- Para redireccionar es fácil, solo pones un a con href con la direccion del archivo, pero voy a intentar eficientarlo 
         ya que solo lo agarra en una parte MUY especifica entonces para q lo agarre mucho mejor-->
         <h2 class=pregunta><strong>¿Qué quieres hacer?<strong></h2>
-        <img src="../../Statics/media/Juegos.jpeg" usemap="#juegos" class="juegos">
+        <a href="./juegos.php"> 
+            <img src="../../Statics/media/Juegos.jpeg" usemap="#juegos" class="juegos">
+        </a>
         <map name="juegos">
             <area target="_self" alt="Juegos" title="Juegos" href="#" coords="3,1,780,862" shape="rect">
         </map>
         <a href="../PHP/Busquedas_principal.php">
-            <img src="../../Statics/media/Busqueda.jpeg" usemap="#busqueda" class="busqueda">
+            <img src="../../Statics/media/Busqueda.jpeg" usemap="#busqueda" class="busqueda">   
         </a>
         <map name="busqueda">
             <area target="_self" alt="busqueda" title="busqueda" href="#" coords="646,14,1,826" shape="rect">
         </map>
-        <img src="../../Statics/media/Compra_y_venta.jpeg" usemap="#compyven" class="comyven">
+        <a href="./ventas.php"> 
+             <img src="../../Statics/media/Compra_y_venta.jpeg" usemap="#compyven" class="comyven">
+        </a>
         <map name="compyven">
             <area target="_self" alt="Comyven" title="Comyven" href="#" coords="636,0,23,857" shape="rect">
         </map>
-        <img src="../../Statics/media/Comunidad.jpeg" usemap="#comunidad" class="comunidad">
+        <a href="#"> 
+            <img src="../../Statics/media/Comunidad.jpeg" usemap="#comunidad" class="comunidad">
+        </a>
         <map name="comunidad">
             <area target="_self" alt="Comunidad" title="Comunidad" href="#" coords="754,864,1,0" shape="rect">
         </map>
