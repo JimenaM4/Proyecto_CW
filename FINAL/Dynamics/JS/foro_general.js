10c4b60ca2acbbc0acb2a8788ef55c1a5105f444
JimenaM4 <133259405+JimenaM4@users.noreply.github.com>
2023-06-26 19:52:35 -0600

    FEAT: añade comentarios

--- FINAL/Dynamics/JS/foro_general.js
+++ FINAL/Dynamics/JS/foro_general.js
@@ -1,39 +1,42 @@
+//Este archivo despliega los registros de foro, por medio de una peticion asincrona
 window.addEventListener("load", ()=>{
     const div = document.getElementById("contpubli");
     const btn_nuevoForo = document.getElementById("btn_nuevoForo");
     let corazonv = "../../Statics/media/foro/coralikeb.png"
     let corazonll = "../../Statics/media/foro/coralikell.png"
-        fetch("./desplegar_foro.php")
-        .then((respuesta)=>{
-            return respuesta.json();
-        }).then((datosJSON)=>{
-             console.log(datosJSON);
-       
-             div.innerHTML = "";
-             datosJSON.forEach((a)=>{
-                console.log(a);
-                let like =a.Likes;
-                // let IDf =a.ID_foro;
-                div.innerHTML +=` <div class="Foros" id="${a.ID_foro}">
-                                    <h1>${a.Titulo}</h1>
-                                    <h2>${a.Tema}</h2>
-                                    <h5>${a.Descripcion}</h5>
-                                    <p>${a.Preguntas}</p>
-                                    <img src="${a.Img}">
-                                    <div id="contlikes">
-                                        <div class="corazon"></div>
-                                        <!--<h2>${like}</h2>-->
-                                        <div class="comentarios"></div>
-                                    </div>
-                                </div>`;
-
-                                
-            });
+    //peticion asincrona que mostrará las publicaciones registradas en la base de datos
+    fetch("./desplegar_foro.php")
+    .then((respuesta)=>{
+        return respuesta.json();
+    }).then((datosJSON)=>{
+        console.log(datosJSON);
+        
+        div.innerHTML = "";
+        datosJSON.forEach((a)=>{
+        console.log(a);
+        let like =a.Likes;
+        // let IDf =a.ID_foro;
+        div.innerHTML +=` <div class="Foros" id="${a.ID_foro}">
+                            <h1>${a.Titulo}</h1>
+                            <h2>${a.Tema}</h2>
+                            <h5>${a.Descripcion}</h5>
+                            <p>${a.Preguntas}</p>
+                            <img src="${a.Img}">
+                            <div id="contlikes">
+                                <div class="corazon"></div>
+                                <!--<h2>${like}</h2>-->
+                                <div class="comentarios"></div>
+                            </div>
+                        </div>`;
+        
+                    
      });
+     });
+    //boton que te redireccionará a crear foro
      btn_nuevoForo.addEventListener("click", ()=>{
          window.location.href = "../../Templates/foro.html";
     });
-
+    
     div.addEventListener("click",(e)=>{
         if(e.target.className=="corazon")
         {
@@ -65,7 +68,5 @@ window.addEventListener("load", ()=>{
             console.log(e.currentTarget.getAttribute("id"));
         }
     });
-
-
 
 });
