d4b9dd3c0ea9b3b3017132e6103d1ef18b8b602b
Manuel V <322229851@alumno.enp.unam.mx>
2023-07-30 14:37:22 -0600

    Feat: agrega base de datos actualizada y su editable

--- FINAL/Docs/Base Datos/resp_proyecto_def.sql
+++ FINAL/Docs/Base Datos/resp_proyecto_def.sql
@@ -61,7 +61,7 @@ CREATE TABLE `juegos` (
   `descripcion` varchar(250) DEFAULT NULL,
   `consola` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`ID_juego`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,6 +70,7 @@ CREATE TABLE `juegos` (
 
 LOCK TABLES `juegos` WRITE;
 /*!40000 ALTER TABLE `juegos` DISABLE KEYS */;
+INSERT INTO `juegos` VALUES (1,'League of Legends(LOL)','TEEN','Lanzamiento: 27 de octubre de 2009','Es un juego de estrategia por equipos en el que dos equipos conformados por cinco poderosos campeones se enfrentan para destruir la base del otro.','Consola: PC');
 /*!40000 ALTER TABLE `juegos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,6 +87,7 @@ CREATE TABLE `mensajes` (
   `ID_foro` int(11) DEFAULT NULL,
   `Contenido` varchar(500) DEFAULT NULL,
   `Likes` int(10) DEFAULT NULL,
+  `FechaHora` char(50) NOT NULL,
   PRIMARY KEY (`ID_mensaje`),
   KEY `ID_usuario` (`ID_usuario`),
   KEY `ID_foro` (`ID_foro`),
@@ -101,6 +103,66 @@ CREATE TABLE `mensajes` (
 LOCK TABLES `mensajes` WRITE;
 /*!40000 ALTER TABLE `mensajes` DISABLE KEYS */;
 /*!40000 ALTER TABLE `mensajes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mensajes_perdidos`
+--
+
+DROP TABLE IF EXISTS `mensajes_perdidos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mensajes_perdidos` (
+  `ID_mensajeP` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_usuario` int(11) DEFAULT NULL,
+  `ID_perdido` int(11) DEFAULT NULL,
+  `contenido` char(250) NOT NULL,
+  `FechaHora` char(50) NOT NULL,
+  PRIMARY KEY (`ID_mensajeP`),
+  KEY `ID_usuario` (`ID_usuario`),
+  KEY `ID_perdido` (`ID_perdido`),
+  CONSTRAINT `mensajes_perdidos_ibfk_1` FOREIGN KEY (`ID_usuario`) REFERENCES `usuario` (`ID_usuario`),
+  CONSTRAINT `mensajes_perdidos_ibfk_2` FOREIGN KEY (`ID_perdido`) REFERENCES `publicar_perdido` (`ID_perdido`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mensajes_perdidos`
+--
+
+LOCK TABLES `mensajes_perdidos` WRITE;
+/*!40000 ALTER TABLE `mensajes_perdidos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mensajes_perdidos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mensajes_ventas`
+--
+
+DROP TABLE IF EXISTS `mensajes_ventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mensajes_ventas` (
+  `ID_mensajeV` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_usuario` int(11) DEFAULT NULL,
+  `ID_venta` int(11) DEFAULT NULL,
+  `contenido` char(250) NOT NULL,
+  `FechaHora` char(50) NOT NULL,
+  PRIMARY KEY (`ID_mensajeV`),
+  KEY `ID_usuario` (`ID_usuario`),
+  KEY `ID_venta` (`ID_venta`),
+  CONSTRAINT `mensajes_ventas_ibfk_1` FOREIGN KEY (`ID_usuario`) REFERENCES `usuario` (`ID_usuario`),
+  CONSTRAINT `mensajes_ventas_ibfk_2` FOREIGN KEY (`ID_venta`) REFERENCES `ventas` (`ID_venta`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mensajes_ventas`
+--
+
+LOCK TABLES `mensajes_ventas` WRITE;
+/*!40000 ALTER TABLE `mensajes_ventas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mensajes_ventas` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -181,6 +243,7 @@ CREATE TABLE `usuario` (
   `usuario` varchar(50) DEFAULT NULL,
   `grupo` int(10) DEFAULT NULL,
   `f_perfil` varchar(250) DEFAULT NULL,
+  `f_portada` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`ID_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -237,4 +300,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-28 13:12:57
+-- Dump completed on 2023-07-30 14:32:22
